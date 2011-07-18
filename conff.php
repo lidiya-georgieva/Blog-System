9ae27d0d9cfad3f021af5e0a63d85ea97a7faecb
unknown <Lidka@.(none)>
2011-07-18 09:21:33 +0300

    conf

--- conff.php
+++ conff.php
@@ -3,7 +3,7 @@
 session_start();
 if(!isset($_SESSION['tema'])){$_SESSION['tema']=1;}
 $salt = "GIF#&IFiy82fgsgfu6&%43";
-$dbcon=mysql_connect("localhost","root","lida900128");
+$dbcon=mysql_connect("localhost","root","");
 if(!$dbcon)
 {
 	die("cannot conect");
